09e5abb3667120bc74814b4dbf17bb89ee3c122d
srini2015 <psjithendra@gmail.com>
2015-09-22 06:48:27 -0400

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## MakeCacheMatrix function stores the Functions
 
 ## Write a short comment describing this function
+## Convert the Matrix to a list Using as.list and store it in x
+## Restore the value of M to Null so that old value can be replaced
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+            x <<- as.list(y)
+            m <<- NULL
+    	}
+	get <- function() x
+	setsolve <- function(solve) m <<- solve
+	getsolve <- function() m
+	 list(set = set, get = get,
+		setsolve = setsolve,
+		getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
+## CacheSolve functions is where MakeCacheMatrix is stored
+## First checks if M value is present, if present provides it else calculates it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
